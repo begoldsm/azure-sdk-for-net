69ef4b73f637f9393d8ab9400827c9e0b8e73065
begoldsm <begoldsm@microsoft.com>
2016-05-06 10:41:56 -0700

    Update test to include logFolderPath

--- src/ResourceManagement/DataLake.Analytics/DataLakeAnalytics.Tests/ScenarioTests/JobOperationTests.cs
+++ src/ResourceManagement/DataLake.Analytics/DataLakeAnalytics.Tests/ScenarioTests/JobOperationTests.cs
@@ -55,7 +55,13 @@ namespace DataLakeAnalytics.Tests
                         // TODO: figure out why this is no longer showing up as a property
                         // Type = JobType.USql, 
                         Script = "DROP DATABASE IF EXISTS testdb; CREATE DATABASE testdb;"
-                    }
+                    }/*,
+                    LogFolder = string.Format("adl://{0}.{1}/some/log/path/", commonData.DataLakeAnalyticsAccountName, clientToUse.AdlaJobDnsSuffix),
+                    LogFilePatterns = new List<string>
+                    {
+                         "*.log",
+                         "*.txt"
+                    }*/
                 };
 
                 var jobCreateResponse = clientToUse.Job.Create(commonData.SecondDataLakeAnalyticsAccountName, jobId, jobToSubmit);
